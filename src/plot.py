51d0fc1bd3ece80d82e6146991e1b6eef6c2f32f
wdecoster <decosterwouter@gmail.com>
2024-02-03 22:56:33 +0100

    working on the collapsed plot

--- src/plot.py
+++ src/plot.py
@@ -67,7 +67,12 @@ def kmer_plot(kmer_df, mode="collapsed", min_length=0):
         kmer_df = kmer_df[kmer_df["length"] >= min_length].drop(columns=["length"])
     else:
         kmer_df = kmer_df.drop(columns=["length"])
-    if mode == "raw" or mode == "collapsed":
+    # sorting the columns based on their total frequency
+    kmer_frequency_sorted = (
+        kmer_df.sum(axis="index").sort_values(ascending=False).index.to_list()
+    )
+    kmer_df = kmer_df[kmer_frequency_sorted]
+    if mode == "raw":
         min_seen_kmer = 0.98 * len(kmer_df.index)
         # only keep columns that are not < 0.01 for too many samples
         mask1 = (kmer_df < 0.01).sum(axis=0) < (min_seen_kmer)
@@ -109,6 +114,68 @@ def kmer_plot(kmer_df, mode="collapsed", min_length=0):
                 height=height,
                 width=height / 4,
             )
+        )
+        fig.update_coloraxes(colorscale="Blues")
+        return fig
+    elif mode == "collapsed":
+        # kmers are thrown out faster
+        min_seen_kmer = 0.90 * len(kmer_df.index)
+        # only keep columns that are not < 0.01 for too many samples
+        mask1 = (kmer_df < 0.05).sum(axis=0) < (min_seen_kmer)
+        # but keep also columns that are above 0.1 for at least 5 samples
+        mask2 = (kmer_df > 0.1).sum(axis=0) > 5
+
+        kmer_df = kmer_df.loc[:, mask1 | mask2]
+
+        # round every kmer count to the nearest 0.1
+        kmer_df = kmer_df.map(lambda x: round(x * 10) / 10)
+        # group all samples together that have identical kmers numbers
+        collapsed = (
+            kmer_df.reset_index()
+            .groupby(kmer_df.columns.to_list())["identifier"]
+            .apply(",".join)
+            .reset_index()
+        )
+        collapsed.to_csv("collapsed.csv", index=False, sep="\t")
+        collapsed["count"] = collapsed["identifier"].apply(lambda x: len(x.split(",")))
+        collapsed = collapsed.sort_values(by="count", ascending=False).reset_index(
+            drop=True
+        )
+        # create a heatmap with a marginal bar chart to indicate the number of carriers
+        fig = make_subplots(
+            rows=1,
+            cols=2,
+            column_widths=[0.8, 0.2],
+            subplot_titles=("Kmer frequency", "Frequency histogram"),
+        )
+
+        fig.add_trace(
+            px.imshow(
+                collapsed.drop(columns=["identifier", "count"]),
+                labels=dict(x="kmers", y="group", color="Frequency"),
+                color_continuous_scale="Blues",
+            ).data[0],
+            row=1,
+            col=1,
+        )
+        fig.add_trace(
+            px.bar(
+                y=collapsed["count"],
+                x=collapsed.index,
+                orientation="h",
+            ).data[0],
+            row=1,
+            col=2,
+        )
+        # make the axis labels a bit smaller
+        fig.update_xaxes(tickfont_size=8, tickangle=45, side="top")
+        fig.update_yaxes(tickfont_size=8)
+        fig.update_layout(
+            {
+                "plot_bgcolor": "rgba(0, 0, 0, 0)",
+                "paper_bgcolor": "rgba(0, 0, 0, 0)",
+                "height": 800,
+            }
         )
         fig.update_coloraxes(colorscale="Blues")
         return fig
