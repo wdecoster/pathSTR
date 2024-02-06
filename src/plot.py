a4ebf104af01b7bfcf95fbe63a2a60a285cf505a
wdecoster <decosterwouter@gmail.com>
2024-02-06 14:37:07 +0100

    limit the violin shape to exactly the points range
    fixing a problem where the log transformed violin went to min inf

--- src/plot.py
+++ src/plot.py
@@ -10,19 +10,15 @@ def violin_plot(filtered_df, log=False, violin_options=None):
         x="Superpopulation" if "population" in violin_options else "gene",
         y="ref_diff" if "ref_diff" in violin_options else "length",
         color="Sex" if "sex" in violin_options else "Group",
+        log_y=log,
         points="all",
         hover_data=["sample"],
     )
-    if log:
-        fig.update_layout(
-            yaxis_type="log",
-            xaxis_title="",
-            yaxis_title="Repeat length [log(units)]",
-        )
-        # fig.update_layout(xaxis_range=[1, filtered_df["length"].max()])
-    else:
-        fig.update_layout(xaxis_title="", yaxis_title="Repeat length [units]")
-    fig.update_traces(marker=dict(size=3))
+    fig.update_layout(
+        xaxis_title="",
+        yaxis_title="Repeat length [log(units)]" if log else "Repeat length [units]",
+    )
+    fig.update_traces(spanmode="hard", marker=dict(size=3))
     if filtered_df["Group"].nunique() > 1 and "sex" not in violin_options:
         fig.update_layout(legend_title_text="Group")
     elif "sex" in violin_options:
